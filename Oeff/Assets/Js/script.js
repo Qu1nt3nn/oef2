2162a38157d5a6496aad03cce7d1a01e0a66417c
Qu1nt3nn <100586837+Qu1nt3nn@users.noreply.github.com>
2022-03-11 21:03:14 +0100

    Add files via upload

---
+++ Oeff/Assets/Js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+    document.querySelector("form").addEventListener("submit",
+        processForm);
+}
+function processForm(e) {
+    e.preventDefault();
+    // YOUR CODE HERE
+}
